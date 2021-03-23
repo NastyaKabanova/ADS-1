ea16f7538ee2ef7a0aec39786307d779a27403b9
Nastya Kabanova <76091946+NastyaKabanova@users.noreply.github.com>
2021-03-23 17:09:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,27 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int s = 0;
+  int h = size - 1;
+  int n = 0;
+  int y = 2;
+  int k = 0;
+
+  while (s < h) {
+      k = (s + h) / y;
+      if (arr[k] < value) {
+          s = k + 1;
+      } else {
+          h = k;
+      }
+  }
+
+  if (arr[s] == value) {
+      while (arr[s] == value) {
+          s++;
+          n++;
+      }
+  }
+
+  return n;
 }
